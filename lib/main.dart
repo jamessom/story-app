3be8bbc954552b373c6de41dad1083de2725d8a9
jamessom <jamessomqueiroz@gmail.com>
2021-03-13 23:59:23 -0300

    feat: add topbar elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'customIcons.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -13,10 +14,39 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    var bgColor = 0xFF2d3447;
+    const bgColor = 0xFF2d3447;
+    const topBarPaddingEdgeInsets =
+        EdgeInsets.only(left: 12.0, right: 12.0, top: 30.0, bottom: 8.0);
+
+    const menuIcon = IconButton(
+      icon: Icon(CustomIcons.menu, color: Colors.white, size: 30.0),
+      onPressed: null,
+    );
+
+    const menuSearch = IconButton(
+      icon: Icon(Icons.search, color: Colors.white, size: 30.0),
+      onPressed: null,
+    );
+
+    var topBarElements = Padding(
+        padding: topBarPaddingEdgeInsets,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[menuIcon, menuSearch],
+        ));
 
     var bodyContent = SingleChildScrollView(
-      child: Column(),
+      child: Column(
+        children: <Widget>[
+          topBarElements,
+          Padding(
+              padding: topBarPaddingEdgeInsets,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[menuIcon, menuSearch],
+              )),
+        ],
+      ),
     );
 
     return Scaffold(
